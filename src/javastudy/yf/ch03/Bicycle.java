ef7ddedd47f2e4cd27294f89c17e1772ea5dec5f
weizhang <wei.zhang3678@gmail.com>
2020-01-18 17:04:37 -0500

    a class example

---
+++ src/javastudy/yf/ch03/Bicycle.java
@@ -0,0 +1,47 @@
+package javastudy.yf.ch03;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 18, 2020 5:03:21 PM
+ * 
+ */
+
+public class Bicycle {
+    
+    // the Bicycle class has
+    // three fields
+    public int cadence;
+    public int gear;
+    public int speed;
+        
+    // the Bicycle class has
+    // one constructor
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+        
+    // the Bicycle class has
+    // four methods
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+        
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+        
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+        
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+        
+}
