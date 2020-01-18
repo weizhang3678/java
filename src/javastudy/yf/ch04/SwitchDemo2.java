308b7b28ee17dd138f1328c6919a9c16cc1cef0c
weizhang <wei.zhang3678@gmail.com>
2020-01-18 17:53:06 -0500

    control flow demoes

---
+++ src/javastudy/yf/ch04/SwitchDemo2.java
@@ -0,0 +1,45 @@
+package javastudy.yf.ch04;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 18, 2020 5:51:02 PM
+ * 
+ */
+
+public class SwitchDemo2 {
+    public static void main(String[] args) {
+
+        int month = 2;
+        int year = 2000;
+        int numDays = 0;
+
+        switch (month) {
+            case 1: case 3: case 5:
+            case 7: case 8: case 10:
+            case 12:
+                numDays = 31;
+                break;
+            case 4: case 6:
+            case 9: case 11:
+                numDays = 30;
+                break;
+            case 2:
+                if (((year % 4 == 0) && 
+                     !(year % 100 == 0))
+                     || (year % 400 == 0))
+                    numDays = 29;
+                else
+                    numDays = 28;
+                break;
+            default:
+                System.out.println("Invalid month.");
+                break;
+        }
+        System.out.println("Number of Days = "
+                           + numDays);
+    }
+}
