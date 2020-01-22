56ed1129aad803876f100dcff97608e9d36753de
weizhang <wei.zhang3678@gmail.com>
2020-01-22 11:35:41 -0500

    add demo

--- src/javastudy/yf/ch03/DoubleDemo.java
+++ src/javastudy/yf/ch03/DoubleDemo.java
@@ -19,6 +19,7 @@ public class DoubleDemo {
 		System.out.println("Double.MAX_VALUE = "+largest);
 		System.out.println("Double.MIN_VALUE = "+smallest);
 		
+		// create Double object
 		Double d1 = 3.0;
 		Double d2 = new Double("3.0qq");
 		Double d3 = new Double(3);
@@ -26,10 +27,36 @@ public class DoubleDemo {
 		System.out.println(d2);
 		System.out.println(d3);
 		
-		Double d4 = Double.valueOf(3.0);
+        // convert double to Double
+		Double d4 = Double.valueOf(1.0);
+		Double d5 = Double.valueOf(1);
+		System.out.println(d4);
+		System.out.println(d5);
+		double d = d5.doubleValue();
+		System.out.println(d);
 		
-		Double d5 = Double.valueOf("3.0");
-		Double d6 = Double.parseDouble("3.0");
+		// convert String to Double
+		Double d6 = Double.valueOf(3.0);		
+		Double d7 = Double.valueOf("3.0");
+		Double d8 = Double.parseDouble("3.0");
+				
+		// convert Double to String
+		System.out.println(d6.toString());
+		System.out.println(String.valueOf(d6));
+		
+		// compare Double
+		Double d9 = new Double(16);
+		Double d10 = new Double(16);
+		System.out.println(d9.equals(d10));
+		System.out.println(d9==d10);
+		System.out.println(d9.intValue()==d10.intValue());	
+		System.out.println(d9.doubleValue()==d10.doubleValue());	
+		System.out.println(d9.compareTo(d10));
+		System.out.println(Double.compare(d9, d10));
+		
+		// NumberFormatException
+		Double d17 = Double.parseDouble("w"+3.0);
+		Double d18 = new Double("w1.0");
 
 		
 		
