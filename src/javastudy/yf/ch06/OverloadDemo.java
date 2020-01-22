022848a099a5b498bd00457d18835f5b1e99f962
weizhang <wei.zhang3678@gmail.com>
2020-01-22 15:37:27 -0500

    writing classes demoes

---
+++ src/javastudy/yf/ch06/OverloadDemo.java
@@ -0,0 +1,29 @@
+package javastudy.yf.ch06;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 18, 2020 5:15:35 PM
+ * 
+ * a demo to illustrate overload
+ * 
+ */
+
+public class OverloadDemo {
+    
+    public void draw(String s) {
+        
+    }
+    public void draw(int i) {
+        
+    }
+    public void draw(double f) {
+        
+    }
+    public void draw(int i, double f) {
+        
+    }
+}
