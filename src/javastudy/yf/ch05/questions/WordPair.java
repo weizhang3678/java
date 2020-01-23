0f1e778290633407b6d31a11b8890900a3bd520a
weizhang <wei.zhang3678@gmail.com>
2020-01-23 10:16:23 -0500

    2018 AP® COMPUTER SCIENCE A FREE-RESPONSE QUESTION 2

---
+++ src/javastudy/yf/ch05/questions/WordPair.java
@@ -0,0 +1,32 @@
+package javastudy.yf.ch05.questions;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 22, 2020 7:23:43 PM
+ * 
+ * 2018 AP® COMPUTER SCIENCE A FREE-RESPONSE QUESTION 2
+ * 
+ */
+
+public class WordPair {
+
+	/** Constructs a WordPair object. */
+	public WordPair(String first, String second) {
+		/* implementation not shown */ }
+
+	/** Returns the first string of this WordPair object. */
+	public String getFirst() {
+		/* implementation not shown */ 
+		return "";
+	}
+
+	/** Returns the second string of this WordPair object. */
+	public String getSecond() {
+		/* implementation not shown */
+		return "";
+	}
+}
---
+++ src/javastudy/yf/ch05/questions/WordPairList.java
@@ -0,0 +1,52 @@
+package javastudy.yf.ch05.questions;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 22, 2020 7:28:43 PM
+ * 
+ * 2018 AP® COMPUTER SCIENCE A FREE-RESPONSE QUESTION 2
+ * 
+ */
+
+public class WordPairList {
+	
+	/** The list of word pairs, initialized by the constructor. */
+	private ArrayList<WordPair> allPairs;
+
+	/**
+	 * Constructs a WordPairList object as described in part (a). * Precondition:
+	 * words.length >= 2
+	 */
+	public WordPairList(String[] words) {
+		allPairs = new ArrayList<WordPair>();
+		/* to be implemented in part (a) */ 
+	     for(int i = 0; i < words.length ; i ++) {
+	    	 String firstWord = words[i];
+	    	 for(int j = i+1; j < words.length ;j ++) {
+	    		 String secondWord = words[j];
+	    		 WordPair wp = new WordPair(firstWord, secondWord);
+	    		 this.allPairs.add(wp);
+	    	 }
+	     }	
+	
+	}
+
+	/** Returns the number of matches as described in part (b). */
+	public int numMatches() {
+		int count = 0;
+		/* to be implemented in part (b) */ 
+	   for(WordPair wp : allPairs) {
+		   if(wp.getFirst().contentEquals(wp.getSecond())) {
+			   count ++;
+		   }
+	   }
+	   
+	   return count;
+	}
+}
