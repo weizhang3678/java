318aa78ff8afabfe42bb6679396f2e3cb29c34cd
weizhang <wei.zhang3678@gmail.com>
2020-01-23 10:14:34 -0500

    2018 AP® COMPUTER SCIENCE A FREE-RESPONSE QUESTION 4

---
+++ src/javastudy/yf/ch08/questions/StringChecker.java
@@ -0,0 +1,16 @@
+package javastudy.yf.ch08.questions;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 22, 2020 7:49:00 PM
+ * 
+ */
+
+public interface StringChecker {
+	/** Returns true if str is valid. */
+	boolean isValid(String str);
+}
