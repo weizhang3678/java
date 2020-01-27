3c2cc4bce8a15be127f58f8184e6e6b170970956
weizhang <wei.zhang3678@gmail.com>
2020-01-27 11:31:43 -0500

    demoes

--- src/javastudy/yf/ch04/Person.java
+++ src/javastudy/yf/ch04/Person.java
@@ -15,22 +15,34 @@ public class Person {
 	String name;
 	int age;
 	
-	
-	
-
-
-
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	// use id,name,age to check equals or not
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
-	@Override
-	// use it to check equals or not
-	public boolean equals(Object obj) {
+
+	// use id to check equals or not
+	public boolean equals_id(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
