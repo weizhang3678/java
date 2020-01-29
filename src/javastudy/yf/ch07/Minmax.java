6e01042ee9df0c3d482f1d67a2c131b49fbe8cc4
weizhang <wei.zhang3678@gmail.com>
2020-01-29 09:49:57 -0500

    min max demo

---
+++ src/javastudy/yf/ch07/Minmax.java
@@ -0,0 +1,29 @@
+package javastudy.yf.ch07;
+
+/**
+ *
+ * Copyright 2020 YF Studio
+ *
+ * All right reserved.
+ * 
+ * Created on Jan 29, 2020 9:47:41 AM
+ * 
+ */
+
+public class Minmax {
+
+	public static void main(String[] args) {
+		int[] values = { 6, 2, 1, 7, 12, 5 };
+		int min = values[0]; // initialize min to the first element
+		int max = values[0];
+		for (int val : values) {
+			if (val < min) // found a new min!
+				min = val;
+			if(val > max)
+				max = val;
+		}
+		System.out.println("Min is " + min);		
+		System.out.println("Max is " + max);
+	}
+
+}
