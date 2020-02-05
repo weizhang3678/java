92d85c7defd1f3493777b70c644c2c821b659417
weizhang <wei.zhang3678@gmail.com>
2020-02-05 13:42:29 -0500

    add

--- src/javastudy/yf/lab/TeleMarketerTest.java
+++ src/javastudy/yf/lab/TeleMarketerTest.java
@@ -34,18 +34,25 @@ public class TeleMarketerTest {
 
 	public static void main(String[] args) {
 		Scanner getInput = new Scanner(System.in);
+		// first line input
 		String first = getInput.nextLine();
+		// second line input
 		String second = getInput.nextLine();
+		// third line input
 		String third = getInput.nextLine();
+		// fourth line input
 		String last = getInput.nextLine();
+		// check four lines inout is valid or not
 		boolean validInputs = isSingleNumber(first) && isSingleNumber(second) && isSingleNumber(third) && isSingleNumber(last);
 		if (!validInputs) {
 			System.out.println("invalid input! You need to input 4 lines, and each line contains exactly one digit in\n" + 
 					"the range from 0 to 9.");
 			getInput.close();
-			return;
+			//return;
+			System.exit(0);
 	        
 		}
+		// check whether input is marketer telephone
 		boolean isMartketer = (first.equals("8") || first.equals("9")) 
 				&& (last.equals("8") || last.equals("9"))
 				&& (second.equals(third));
