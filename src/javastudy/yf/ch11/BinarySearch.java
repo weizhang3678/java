1389f20f93002d25a21c1c8bee941c2b15ab5978
weizhang <wei.zhang3678@gmail.com>
2020-02-08 14:05:47 -0500

    recursive binary search

---
+++ src/javastudy/yf/ch11/BinarySearch.java
@@ -0,0 +1,28 @@
+package javastudy.yf.ch11;
+
+import java.util.ArrayList;
+
+public class BinarySearch {
+
+	public static int binarySearch(int[] arr, int left, int right, int target) {
+		int middle = (left + right) / 2;
+		if (right <= left) {
+			return -1;
+		}
+		if (arr[middle] == target) {
+			return middle;
+		} else if (arr[middle] > target) {
+			return binarySearch(arr, left, middle-1, target);
+
+		} else {
+			return binarySearch(arr, middle+1, right, target);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 6, 8, 10, 11, 23, 45, 67, 77, 79, 80, 99, 101 };
+		System.out.println(binarySearch(arr, 0, arr.length, 77));
+
+	}
+
+}
