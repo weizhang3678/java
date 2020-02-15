c3de094c355a2d1db7c77fbef3331d1a677e9034
weizhang <wei.zhang3678@gmail.com>
2020-02-15 17:39:27 -0500

    commit

--- src/javastudy/yf/ch01/HelloWorld.java
+++ src/javastudy/yf/ch01/HelloWorld.java
@@ -8,7 +8,7 @@ package javastudy.yf.ch01;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello, World " + (args.length > 0 ? args[0] : ""));
 
 	}
